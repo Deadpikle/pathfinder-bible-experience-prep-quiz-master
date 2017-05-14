6fd605eca2eafc185632c074b59135f7c5f52057
Deadpikle <mikebobby@sbcglobal.net>
2017-05-14 17:37:57 -0400

    Forgot to add a file

---
+++ admin/ajax/save-user-edits.php
@@ -0,0 +1,30 @@
+<?php
+    require_once(dirname(__FILE__)."/../init-admin.php");
+
+    if (!isset($_POST["is-admin"])) {
+        $_POST["is-admin"] = FALSE;
+    }
+    $params = [
+        $_POST["first-name"],
+        $_POST["last-name"],
+        $_POST["entry-code"],
+        $_POST["is-admin"]
+    ];
+    if ($_GET["type"] == "update") {
+        $query = '
+            UPDATE Users SET FirstName = ?, LastName = ?, EntryCode = ?, IsAdmin = ? WHERE UserID = ?
+        ';
+        $params[] = $_POST["user-id"];
+    }
+    else if ($_GET["type"] == "create") {
+        $query = '
+            INSERT INTO Users (FirstName, LastName, EntryCode, IsAdmin) VALUES (?, ?, ?, ?)
+        ';
+    }
+    else {
+        die("Invalid type");
+    }
+    $stmt = $pdo->prepare($query);
+    $stmt->execute($params);
+    header("Location: $basePath/admin/view-users.php");
+?>
