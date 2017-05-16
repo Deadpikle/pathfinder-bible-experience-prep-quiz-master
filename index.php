238c748a1c71679d45830b11fbff775185d0960c
Deadpikle <mikebobby@sbcglobal.net>
2017-05-16 18:30:30 -0400

    Create page for quiz setup

--- index.php
+++ index.php
@@ -12,7 +12,15 @@
     <ul>
         <li><a href="add-edit-question.php?type=create">Add Question</a></li>
         <li><a href="view-questions.php">View Questions</a></li>
+        <li><a href="quiz-setup.php">Quiz me!</a></li>
     </ul>  
 </div>
+<?php if ($_SESSION["IsAdmin"]) { ?>
+    <div id="admin-links">
+        <ul>
+            <li><a href="admin">Admin Panel</a></li>
+        </ul>  
+    </div>
+<?php } ?>
 
 <?php include(dirname(__FILE__)."/footer.php") ?>
---
+++ quiz-setup.php
@@ -0,0 +1,13 @@
+<?php
+
+    require_once(dirname(__FILE__)."/init.php");
+
+?>
+
+<?php include(dirname(__FILE__)."/header.php"); ?>
+
+<div id="start-quiz">
+    <h4>Quiz Setup</h4>
+</div>
+
+<?php include(dirname(__FILE__)."/footer.php") ?>
