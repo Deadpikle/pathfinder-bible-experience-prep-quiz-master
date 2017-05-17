5e16cd46195fcbc631775617eeaa9f2e3c9c3328
Deadpikle <mikebobby@sbcglobal.net>
2017-05-16 21:20:59 -0400

    Try/catch is save edits

--- admin/ajax/save-user-edits.php
+++ admin/ajax/save-user-edits.php
@@ -1,30 +1,35 @@
 <?php
     require_once(dirname(__FILE__)."/../init-admin.php");
-
-    if (!isset($_POST["is-admin"])) {
-        $_POST["is-admin"] = FALSE;
+    try {
+        if (!isset($_POST["is-admin"])) {
+            $_POST["is-admin"] = FALSE;
+        }
+        $params = [
+            $_POST["first-name"],
+            $_POST["last-name"],
+            $_POST["entry-code"],
+            $_POST["is-admin"]
+        ];
+        if ($_GET["type"] == "update") {
+            $query = '
+                UPDATE Users SET FirstName = ?, LastName = ?, EntryCode = ?, IsAdmin = ? WHERE UserID = ?
+            ';
+            $params[] = $_POST["user-id"];
+        }
+        else if ($_GET["type"] == "create") {
+            $query = '
+                INSERT INTO Users (FirstName, LastName, EntryCode, IsAdmin) VALUES (?, ?, ?, ?)
+            ';
+        }
+        else {
+            die("Invalid type");
+        }
+        $stmt = $pdo->prepare($query);
+        $stmt->execute($params);
+        header("Location: $basePath/admin/view-users.php");
     }
-    $params = [
-        $_POST["first-name"],
-        $_POST["last-name"],
-        $_POST["entry-code"],
-        $_POST["is-admin"]
-    ];
-    if ($_GET["type"] == "update") {
-        $query = '
-            UPDATE Users SET FirstName = ?, LastName = ?, EntryCode = ?, IsAdmin = ? WHERE UserID = ?
-        ';
-        $params[] = $_POST["user-id"];
+    catch (PDOException $e) {
+        print_r($e);
+        die();
     }
-    else if ($_GET["type"] == "create") {
-        $query = '
-            INSERT INTO Users (FirstName, LastName, EntryCode, IsAdmin) VALUES (?, ?, ?, ?)
-        ';
-    }
-    else {
-        die("Invalid type");
-    }
-    $stmt = $pdo->prepare($query);
-    $stmt->execute($params);
-    header("Location: $basePath/admin/view-users.php");
 ?>
