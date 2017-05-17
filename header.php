a10edf5bb21887deaa0b530a81e648b5ca2db40b
Deadpikle <mikebobby@sbcglobal.net>
2017-05-17 07:49:36 -0400

    Add normalize.css

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 
 <html>
     <head>
+        <link rel="stylesheet" href="<?=$basePath?>/css/normalize.css" />
         <link rel="stylesheet" href="<?=$basePath?>/css/common.css" />
         <script src="<?=$basePath?>/lib/jquery-3.2.1.min.js"></script>
     </head>
