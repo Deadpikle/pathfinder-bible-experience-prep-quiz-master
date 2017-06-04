fdec444451fec8f71b1a892fc412a8fe73122346
Deadpikle <mikebobby@sbcglobal.net>
2017-06-04 17:06:34 -0400

    Basic quiz taking now working

--- quiz.php
+++ quiz.php
@@ -43,8 +43,38 @@
     </div>
 </div>
 
+<div id="take-quiz">
+    <h5 id="question-text">Question:</h5>
+    <div class="row">
+        <div class="input-field col s12 m6">
+            <input type="text" id="quiz-answer" name="quiz-answer" required/>
+            <label for="quiz-answer">Answer</label>
+        </div>
+            <div class="input-field col s6 m6">
+                <button id="check-answer" class="btn btn-flat blue white-text waves-effect blue-waves">Check answer</button>
+            </div>
+    </div>
+    <p id="question-result-correct">That's the right answer! Good job!</p>
+    <p id="question-result-wrong">Sorry, that's not the correct answer.</p>
+    <button id="flag-question" class="btn btn-flat blue white-text waves-effect blue-waves">Flag question</button>
+    <button id="next-question" class="btn btn-flat blue white-text waves-effect blue-waves">Next question</button>
+</div>
+
 <script type="text/javascript">
     $(document).ready(function() {
+        var currentQuestionIndex = 0;
+        var questions = [];
+        var currentQuestion = null;
+
+        var $correctAnswerText = $("#question-result-correct");
+        var $incorrectAnswerText = $("#question-result-wrong");
+
+        var flagQuestion = document.getElementById('flag-question');
+
+        flagQuestion.addEventListener('click', function() {
+            // TODO: allow for flagging a question
+        }, false);
+
         function loadQuiz() {
             $("#loading-quiz").show();
             $.ajax({
@@ -58,18 +88,57 @@
                     shouldAvoidPastCorrect: shouldAvoidPastCorrect
                 },
                 success: function(response) {
-                    var data = JSON.parse(response);
-                    // create quiz for user...
+                    if (response.questions.length > 0) {
+                        questions = response.questions;
+                        currentQuestionIndex = 0;
+                        showQuestionAtCurrentIndex();
+                    }
+                    else {
+                        // no questions! user is done with all questions and should probably reset their saved question answers
+                    }
+
                     $("#loading-quiz").hide();
                 },
                 error: function (xhr, ajaxOptions, thrownError) {
-                    /*var $questionsBody = $("#questions-body");
-                    $questionsBody.empty();
-                    previousPage.disabled = true;
-                    nextPage.disabled = true;*/
                     alert("Unable to generate quiz. Please make sure you are connected to the internet or try again later.");
                 }
             });
+        }
+
+        function checkUserAnswer() {
+            var answer = $("#quiz-answer").val();
+            if (answer == currentQuestion.answer) {
+                $correctAnswerText.show();
+                $incorrectAnswerText.hide();
+            }
+            else {
+                $correctAnswerText.hide();
+                $incorrectAnswerText.show();
+            }
+        }
+
+        var checkAnswer = document.getElementById('check-answer');
+        checkAnswer.addEventListener('click', function() {
+            checkUserAnswer();
+            nextQuestion.disabled = false;
+        }, false);
+
+        function moveToNextQuestion() {
+            currentQuestionIndex++;
+            showQuestionAtCurrentIndex();
+        }
+
+        var nextQuestion = document.getElementById('next-question');
+        nextQuestion.addEventListener('click', function() {
+            moveToNextQuestion();
+        }, false);
+
+        function showQuestionAtCurrentIndex() {
+            $correctAnswerText.hide();
+            $incorrectAnswerText.hide();
+            nextQuestion.disabled = true;
+            currentQuestion = questions[currentQuestionIndex];
+            $("#question-text").html(currentQuestion.question);
         }
 
         loadQuiz();
