a16d56a8cd0fd2119d1aabe635a9ee259f90029a
Deadpikle <mikebobby@sbcglobal.net>
2017-06-25 14:48:22 -0400

    Generate quiz with commentary questions

--- ajax/generate-quiz.php
+++ ajax/generate-quiz.php
@@ -39,21 +39,22 @@
     // fill-in-only
     $questionTypes = $_POST["questionTypes"]; // TODO: fill in the blank
     $questionOrder = $_POST["questionOrder"];
-    $isRandomOrder = $questionOrder == "random-sequential" || $questionOrder == "random-random";
-    $mustSortAfterQuery = $questionOrder == "random-sequential";
+    $areRandomQuestionsPulled = $questionOrder == "random-sequential" || $questionOrder == "random-random";
+    $isOutputSequential = $questionOrder == "random-sequential" || $questionOrder == "sequential-sequential";
     $shouldAvoidPastCorrect = $_POST["shouldAvoidPastCorrect"]; // TODO: extra JOIN and WHERE 
 
     // Setup query
     $selectPortion = '
-        SELECT q.QuestionID, Question, Answer, NumberPoints, DateCreated,
+        SELECT q.QuestionID, q.Type, Question, Answer, NumberPoints, DateCreated,
             bStart.Name AS StartBook, cStart.Number AS StartChapter, vStart.Number AS StartVerse,
             bEnd.Name AS EndBook, cEnd.Number AS EndChapter, vEnd.Number AS EndVerse,
-            IFNULL(uf.UserFlaggedID, 0) AS IsFlagged ';
+            IFNULL(uf.UserFlaggedID, 0) AS IsFlagged,
+            CommentaryVolume, CommentaryStartPage, CommentaryEndPage ';
     $fromPortion = '
         FROM Questions q 
-            JOIN Verses vStart ON q.StartVerseID = vStart.VerseID
-            JOIN Chapters cStart on vStart.ChapterID = cStart.ChapterID
-            JOIN Books bStart ON bStart.BookID = cStart.BookID
+            LEFT JOIN Verses vStart ON q.StartVerseID = vStart.VerseID
+            LEFT JOIN Chapters cStart on vStart.ChapterID = cStart.ChapterID
+            LEFT JOIN Books bStart ON bStart.BookID = cStart.BookID
 
             LEFT JOIN Verses vEnd ON q.EndVerseID = vEnd.VerseID 
             LEFT JOIN Chapters cEnd on vEnd.ChapterID = cEnd.ChapterID 
@@ -62,29 +63,96 @@
     $whereClause = ' 
         WHERE NumberPoints <= ' . $maxPoints;
     $orderByPortion = '';
-    if ($isRandomOrder) {
+    if ($areRandomQuestionsPulled) {
         $orderByPortion = ' ORDER BY RAND() ';
     }
     else {
         // sequential-sequential
+        // this is less than ideal due to the commentary vs Bible q&a ordering, but 
+        // I couldn't think of a better & quick way to handle sequential-sequential with multiple question types
+        // I really need to pull these out with 2 queries, but that's problematic due to max questions.
+        // Should I just enforce a start/end verse on commentary questions?
+        // sequential-sequential is problematic when pulling questions out of two locations...
         $orderByPortion = '
-            ORDER BY bStart.Name, cStart.Number, vStart.Number, bEnd.Name, cEnd.Number, vEnd.Number';
+            ORDER BY COALESCE(bStart.Name, cStart.Number, vStart.Number, bEnd.Name, cEnd.Number, vEnd.Number,
+                CommentaryVolume, CommentaryStartPage, CommentaryEndPage)';
     }
 
     $limitPortion = ' LIMIT ' . $maxQuestions;
     $stmt = $pdo->query($selectPortion . $fromPortion . $whereClause . $orderByPortion . $limitPortion);
     $questions = $stmt->fetchAll();
-    if ($mustSortAfterQuery) {
+
+
+    if ($isOutputSequential) {
+        // If things need to be shown sequentially, we need to separate the question types, sort them individually,
+        // then re-merge them in a random order (but still sequential within the question types)
+
+        // Separate the two question types
+        $bibleQnA = array();
+        $commentaryQnA = array();
+        foreach ($questions as $question) {
+            $type = $question["Type"];
+            if ($type === "bible-qna") {
+                $bibleQnA[] = $question;
+            }
+            else if ($type === "commentary-qna") {
+                $commentaryQnA[] = $question;
+            }
+        }
+
+        // Sort the arrays
         // https://stackoverflow.com/a/3233009/3938401
         array_multisort(
-            array_column($questions, 'StartBook'), SORT_ASC, 
-            array_column($questions, 'StartChapter'), SORT_ASC, 
-            array_column($questions, 'StartVerse'), SORT_ASC,
-            array_column($questions, 'EndBook'), SORT_ASC,
-            array_column($questions, 'EndChapter'), SORT_ASC,
-            array_column($questions, 'EndVerse'), SORT_ASC,
-            $questions);
+            array_column($bibleQnA, 'StartBook'), SORT_ASC, 
+            array_column($bibleQnA, 'StartChapter'), SORT_ASC, 
+            array_column($bibleQnA, 'StartVerse'), SORT_ASC,
+            array_column($bibleQnA, 'EndBook'), SORT_ASC,
+            array_column($bibleQnA, 'EndChapter'), SORT_ASC,
+            array_column($bibleQnA, 'EndVerse'), SORT_ASC,
+            $bibleQnA);
+        array_multisort(
+            array_column($commentaryQnA, 'CommentaryVolume'), SORT_ASC, 
+            array_column($commentaryQnA, 'CommentaryStartPage'), SORT_ASC, 
+            array_column($commentaryQnA, 'CommentaryEndPage'), SORT_ASC,
+            $commentaryQnA);
+
+        // Merge them back together~
+        $output = array();
+        $bibleCount = count($bibleQnA);
+        $commentaryCount = count($commentaryQnA);
+        $totalQuestions = $bibleCount + $commentaryCount;
+        $bibleIndex = 0;
+        $commentaryIndex = 0;
+        for ($i = 0; $i < $totalQuestions; $i++) {
+            // 0 = Bible question, 1 = commentary question obtained via random_int(0, 1);
+            $hasBibleQuestionLeft = $bibleIndex < $bibleCount;
+            $hasCommentaryQuestionLeft = $commentaryIndex < $commentaryCount;
+            if ($hasBibleQuestionLeft && $hasCommentaryQuestionLeft) {
+                // pull next one out randomly
+                $randomSelection = random_int(0, 1);
+                if ($randomSelection == 0) {
+                    $output[] = $bibleQnA[$bibleIndex];
+                    $bibleIndex++;
+                }
+                else {
+                    $output[] = $commentaryQnA[$commentaryIndex];
+                    $commentaryIndex++;
+                }
+            }
+            else if ($hasBibleQuestionLeft) {
+                $output[] = $bibleQnA[$bibleIndex];
+                $bibleIndex++;
+            }
+            else {
+                // has commentary question left
+                $output[] = $commentaryQnA[$commentaryIndex];
+                $commentaryIndex++;
+            }
+        }
+        // set questions to output of this algorithm~
+        $questions = $output;
     }
+    // TODO: pull out fill in the blank questions
     // TODO: sort/merge with fill in the blank questions
 
     // Generate output
@@ -92,22 +160,30 @@
     $number = 1;
     foreach ($questions as $question) {
         $data = array (
-            "type" => "qa", // todo: fill in the blank
+            "type" => $question["Type"], // todo: fill in the blank
             "number" => $number,
             "id" => $question["QuestionID"],
             "isFlagged" => $question["IsFlagged"],
             "points" => $question["NumberPoints"],
-            "startBook" => $question["StartBook"],
-            "startChapter" => $question["StartChapter"],
-            "startVerse" => $question["StartVerse"],
-            "endBook" => $question["EndBook"] != NULL ? $question["EndBook"] : "",
-            "endChapter" => $question["EndChapter"] != NULL ? $question["EndChapter"] : "",
-            "endVerse" => $question["EndVerse"] != NULL ? $question["EndVerse"] : "",
-            //
             "question" => $question["Question"],
             "answer" => $question["Answer"]
-            // for fill in the blank, will have text/blank key/value pairs
         );
+        if ($question["Type"] == "bible-qna") {
+            // Bible Q&A
+            $data["startBook"] = $question["StartBook"] != NULL ? $question["StartBook"] : "";
+            $data["startChapter"] = $question["StartChapter"] != NULL ? $question["StartChapter"] : "";
+            $data["startVerse"] = $question["StartVerse"] != NULL ? $question["StartVerse"] : "";
+            $data["endBook"] = $question["EndBook"] != NULL ? $question["EndBook"] : "";
+            $data["endChapter"] = $question["EndChapter"] != NULL ? $question["EndChapter"] : "";
+            $data["endVerse"] = $question["EndVerse"] != NULL ? $question["EndVerse"] : "";
+        }
+        else if ($question["Type"] == "commentary-qna") {
+            // commentary Q&A
+            $data["volume"] = $question["CommentaryVolume"];
+            $data["startPage"] = $question["CommentaryStartPage"];
+            $data["endPage"] = $question["CommentaryEndPage"];
+        }
+        // for fill in the blank, will have text/blank key/value pairs
         $outputQuestions[] = $data;
         $number++;
     }
