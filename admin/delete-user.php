4647056c937a15a874449bcdc4d16e70e03d5cfb
Deadpikle <mikebobby@sbcglobal.net>
2017-06-25 19:05:56 -0400

    Fix delete user bug

--- admin/delete-user.php
+++ admin/delete-user.php
@@ -3,7 +3,7 @@
 
     $userID = $_GET["id"];
 
-    $query = 'SELECT FirstName, LastName FROM Users WHERE UserID = ?';
+    $query = 'SELECT Username FROM Users WHERE UserID = ?';
     $stmt = $pdo->prepare($query);
     $stmt->execute([$userID]);
     $user = $stmt->fetch();
@@ -25,7 +25,7 @@
 <p><a href="./view-users.php">Back</a></p>
 
 <div id="delete-user">
-    <h4> Are you sure you want to delete <?= $user["FirstName"] ?> <?= $user["LastName"] ?>? </h4>
+    <h4> Are you sure you want to delete <?= $user["Username"] ?>? </h4>
     <form method="post">
         <input type="hidden" name="user-id" value="<?= $userID ?>"/>
         <button class="btn waves-effect waves-light submit red white-text" type="submit" name="action">Delete User</button>
