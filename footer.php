ad9e41a5bdd9edec482fafcefc773ee0bddd1228
Deadpikle <mikebobby@sbcglobal.net>
2017-06-25 11:17:07 -0400

    Remove 2 superscript from footer

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
                 </div>
             </main>
         <footer class="container">
-            <p><a class="" href="<?=$basePath?>/about.php">UCC Quiz Engine<sup>2</sup></a> © 2017</p>
+            <p><a class="" href="<?=$basePath?>/about.php">UCC Quiz Engine</a> © 2017</p>
         </footer>
     </body>
     <script type="text/javascript">
