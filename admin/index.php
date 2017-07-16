3a20de9677cfa86222d69fdfc26aebca7ad5caeb
Deadpikle <mikebobby@sbcglobal.net>
2017-07-16 17:52:22 -0400

    Separate out club and web admin functions

--- admin/index.php
+++ admin/index.php
@@ -7,14 +7,18 @@
 <h2>Administrator Panel</h2>
 
 <div id="admin-links">
+    <h4>Club Administrator Functions</h4>
     <ul>
         <li class="home-buttons"><a class="btn waves-effect waves-light" href="view-users.php">Users</a></li>
-        <?php if ($isWebAdmin) { ?>
-            <li class="home-buttons"><a class="btn waves-effect waves-light" href="view-clubs.php">Pathfinder Clubs</a></li>
-            <li class="home-buttons"><a class="btn waves-effect waves-light" href="view-home-sections.php">Home Page Info</a></li>
-            <li class="home-buttons"><a class="btn waves-effect waves-light" href="manage-study-guides.php">Manage Study Guides</a></li>
-        <?php } ?>
     </ul>  
+    <?php if ($isWebAdmin) { ?>
+        <h4>Web Administrator Functions</h4>
+        <ul>
+            <li class="home-buttons"><a class="btn waves-effect waves-light" href="view-home-sections.php">Home Page Info</a></li>
+            <li class="home-buttons"><a class="btn waves-effect waves-light" href="view-clubs.php">Pathfinder Clubs</a></li>
+            <li class="home-buttons"><a class="btn waves-effect waves-light" href="manage-study-guides.php">Study Guides</a></li>
+        </ul>
+    <?php } ?>
 </div>
 
 <?php include(dirname(__FILE__)."/../footer.php") ?>
