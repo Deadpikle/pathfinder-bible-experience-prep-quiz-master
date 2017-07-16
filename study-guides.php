943b9510847465c57fc763410b485158a1acea1e
Deadpikle <mikebobby@sbcglobal.net>
2017-07-16 19:42:41 -0400

    Show message if no study guides

--- study-guides.php
+++ study-guides.php
@@ -14,13 +14,17 @@
 <h2>Study Guides</h2>
 
 <div id="view-study-guides">
-    <ul class="browser-default">
-        <?php foreach ($files as $file) { ?>
-            <li>
-                <td><a target="_blank" class="btn-flat blue-text waves-effect waves-blue no-uppercase" href="<?= $basePath . '/' .$file['FileName'] ?>"><?= $file['DisplayName'] ?></a></td> 
-            </li>
-        <?php } ?>
-    </ul>
+    <?php if (count($files) === 0) { ?>
+        <p>There are no study guides available at this time.</p>
+    <?php } else { ?>
+        <ul class="browser-default">
+            <?php foreach ($files as $file) { ?>
+                <li>
+                    <td><a target="_blank" class="btn-flat blue-text waves-effect waves-blue no-uppercase" href="<?= $basePath . '/' .$file['FileName'] ?>"><?= $file['DisplayName'] ?></a></td> 
+                </li>
+            <?php } ?>
+        </ul>
+    <?php } ?>
 </div>
 
 <?php include(dirname(__FILE__)."/footer.php") ?>
