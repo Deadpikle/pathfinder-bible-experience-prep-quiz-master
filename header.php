6d5eb92c8016adac3a23948772766542109af159
Deadpikle <mikebobby@sbcglobal.net>
2017-08-03 18:59:37 -0400

    Update materialize; refactor autosize to its own file

--- header.php
+++ header.php
@@ -19,6 +19,7 @@
         <script src="<?=$basePath?>/lib/jquery-3.2.1.min.js"></script>
         <script type="text/javascript" src="<?=$basePath?>/lib/materialize/js/materialize.min.js"></script>
         <script type="text/javascript" src="<?=$basePath?>/lib/html.sortable.min.js"></script> <!-- https://github.com/lukasoppermann/html5sortable -->
+        <script type="text/javascript" src="<?=$basePath?>/lib/autosize.min.js"></script>
         <script src="<?=$basePath?>/js/common.js?v=20170525"></script>
     </head>
     <body>
