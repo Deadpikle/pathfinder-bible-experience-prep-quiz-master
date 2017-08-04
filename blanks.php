4f89961109b8f70ca10e51058f224df4ccd9febb
Deadpikle <mikebobby@sbcglobal.net>
2017-08-03 21:21:52 -0400

    Some small algorithm tweaks to blank.php Fix a bug where 'a' could match as blankable

--- blanks.php
+++ blanks.php
@@ -1,26 +1,15 @@
 <?php
+// blanks a minimum of 1 word
 	require_once('./test.php');
 
 	const SKIPPABLE = ['a', 'is', 'and', 'or', 'but', 'the'];
     const PUNCTUATION = ['.', '?', '!', ',', ' '];
 
-	/*
-	 * words = verse
-	 * |> tokenize
-	 *
-	 * blanks = words
-	 * |> get_indexes
-	 * |> filter_trivial
-	 * |> shuffle
-	 * |> take(n)
-	 *
-	 * result = words
-	 * |> replace(blanks)
-	 * |> concat
-	 */
-
     // $percentToBlank should be a decimal
 	function generate_question($phrase, $percentToBlank) {
+        echo '<br>-----<br>';
+        echo $phrase;
+        echo '<br>';
 		$tokenized = tokenize($phrase);
         // echo $phrase;
         // echo '<br>';
@@ -31,6 +20,9 @@
         $blankableIndices = $tokenized["blankable-indices"];
         $numberWords = count($data);
         $numberToBlank = floor($percentToBlank * count($blankableIndices));
+        if ($numberToBlank == 0 && $percentToBlank >= 0.0) {
+            $numberToBlank = 1;
+        }
 
         // get items we should blank
 		shuffle($blankableIndices);
@@ -38,11 +30,10 @@
         for ($i = 0; $i < count($blankableIndices); $i++) {
             $data[$blankableIndices[$i]]["shouldBeBlanked"] = TRUE;
         }
-        echo '<br>-----<br>';
-        echo $phrase;
+        echo $numberToBlank . ' to blank';
         echo '<br>';
         print_r($data);
-        echo '<br>';
+        echo '<br>-----<br>';
         return $data;
 	}
 
@@ -53,10 +44,13 @@
         $blankableIndices = [];
         for ($i = 0; $i < count($words); $i++) {
             $word = $words[$i];
+            // could add ^' to keep words like 'taint (as in "'taint so") in the word section and not in the before section
             preg_match("/^([^\w]*)(.*?)([^\w]*)$/", $word, $matches);
             $actualWord = trim($matches[2]);
-            $isBlankable = array_search($actualWord, SKIPPABLE) == FALSE ? TRUE : FALSE;
+            $isBlankable = array_search($actualWord, SKIPPABLE) === FALSE ? TRUE : FALSE;
             if ($isBlankable) {
+                echo '"' . $actualWord . '" is blankable';
+                echo '<br>';
                 $blankableIndices[] = $i;
             }
             $word_array = [
@@ -74,114 +68,8 @@
         ];
     }
 
-    // technically this works, but it is much less awesome than @stephenwade's regex solution
-    /*
-	function tokenize($phrase) {
-		$words = explode(' ', $phrase);
-		$result = [];
-
-		foreach ($words as $word) {
-            $strlen = strlen($word);
-            $isSearchingBefore = TRUE;
-            $isReadingWord = FALSE;
-            $isSearchingAfter = FALSE;
-            $beforePunctuation = "";
-            $afterPunctuation = "";
-            $actualWord = "";
-            for ($i = 0; $i < strlen($word); $i++) {
-                $char = $word[$i];
-                // $char contains the current character, so do your processing here
-                $key = array_search($char, PUNCTUATION);
-                //echo $key . ' ';
-                $isPunct = $key !== FALSE;
-                if ($isSearchingBefore && $isPunct) {
-                    $beforePunctuation .= $char;
-                }
-                else if ($isSearchingBefore && !$isPunct) {
-                    $isSearchingBefore = FALSE;
-                    $isReadingWord = TRUE;
-                    $actualWord .= $char;
-                }
-                else if ($isReadingWord && !$isPunct) {
-                    $actualWord .= $char;
-                }
-                else {
-                    $isReadingWord = FALSE;
-                    $isSearchingAfter = TRUE;
-                    $afterPunctuation .= $char;
-                }
-                //echo '[key= ' . $key . ', ispunct = ' . $isPunct . '][' . $beforePunctuation . '][' . $actualWord . '][' . $afterPunctuation . ']<br>';
-            }
-            $innerResult = [
-                "before" => "",
-                "word" => "",
-                "after" => "",
-                "blankable" => FALSE
-            ];
-            $didAdd = FALSE;
-            if ($beforePunctuation !== "") {
-                $innerResult["before"] = $beforePunctuation;
-                $didAdd = TRUE;
-                //echo 'adding before - ';
-            }
-            if ($actualWord !== "") {
-                $innerResult["word"] = trim($actualWord);
-                $didAdd = TRUE;
-                if (array_search($actualWord, SKIPPABLE) !== FALSE) {
-                    $innerResult["blankable"] = true;
-                }
-                //echo 'adding actual - ';
-            }
-            if ($afterPunctuation !== "") {
-                $innerResult["after"] = $afterPunctuation;
-                $didAdd = TRUE;
-                //echo 'adding after - ';
-            }
-            if ($didAdd) {
-                $result[] = $innerResult;
-            }
-            //echo '<br>';
-		}
-		return $result;
-	}*/
-
-	function condense($tokens) {
-		$result = [];
-		$acc = '';
-
-		foreach ($tokens as $token) {
-			if ($token == '_') {
-				if ($acc) {
-					$result[] = $acc;
-					$acc = '';
-				}
-				$result[] = $token;
-			} else {
-				$acc = $acc ? ($acc . ' ' . $token) : $token;
-			}
-		}
-
-		if ($acc) {
-			$result[] = $acc;
-		}
-
-		return $result;
-	}
-
-	function get_blanks($tokens, $num_blanks) {
-		$keys = array_keys($tokens);
-
-		$blankable = array_filter($keys, function ($key) use ($tokens) {
-			return !in_array($tokens[$key], SKIPPABLE);
-		});
-
-		shuffle($blankable);
-
-		return array_slice($blankable, 0, $num_blanks);
-	}
-
 	// #generate_question
-	function test_generate_question() {
+	/*function test_generate_question() {
 		$t = new Test();
 
 		// replaces a word with a blank
@@ -193,20 +81,20 @@
 		$t->equal(generate_question("Hello!", 1), ['_', '!']);
 
 		// doesn't choke on multiple words
-		$t->equal(generate_question("Hello world!", 2), ['_', '_', '!']);
-		$t->equal(generate_question("Hello, world!", 2), ['_', ',', '_', '!']);
+		$t->equal(generate_question("Hello world!", 0.5), ['_', '_', '!']);
+		$t->equal(generate_question("Hello, world!", 0.5), ['_', ',', '_', '!']);
 
         // multiple punctuation at end
-		$t->equal(generate_question("Hello, world!?!", 2), ['_', ',', '_', '!?!']);
+		$t->equal(generate_question("Hello, world!?!", 0.5), ['_', ',', '_', '!?!']);
 
 		// ignores conjunctions
-		$t->equal(generate_question("Fred and Harry.", 2), ['_', 'and', '_', '.']);
+		$t->equal(generate_question("Fred and Harry.", 0.5), ['_', 'and', '_', '.']);
 
 		// Condenses consecutive trivial tokens
-		$t->equal(generate_question("This is a pen.", 2), ['_', 'is a', '_', '.']);
+		$t->equal(generate_question("This is a pen.", 0.75), ['_', 'is a', '_', '.']);
 
 		$t->print_results();
 	}
 
-	test_generate_question();
+	test_generate_question();*/
 ?>
