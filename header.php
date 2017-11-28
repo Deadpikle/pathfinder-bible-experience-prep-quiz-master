de65d72ebbb5ceb173135b16f8c1f766c01edd6e
Deadpikle <mikebobby@sbcglobal.net>
2017-11-27 19:32:15 -0500

    Piwik stuff in quotes

--- header.php
+++ header.php
@@ -43,9 +43,9 @@
                 _paq.push(['trackPageView']);
                 _paq.push(['enableLinkTracking']);
                 (function() {
-                    var u = <?= $analyticsURL ?>;
+                    var u = "<?= $analyticsURL ?>";
                     _paq.push(['setTrackerUrl', u+'piwik.php']);
-                    _paq.push(['setSiteId', <?= $analyticsSiteID ?>]);
+                    _paq.push(['setSiteId', "<?= $analyticsSiteID ?>"]);
                     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
                     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
                 })();
