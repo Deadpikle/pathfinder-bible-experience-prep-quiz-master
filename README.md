238ef32b4ab7426ed78551022717536b04704cdb
Deadpikle <mikebobby@sbcglobal.net>
2018-02-08 21:56:55 -0500

    Start working on initial data seed

--- README.md
+++ README.md
@@ -8,5 +8,6 @@
 4. `vendor/bin/phinx migrate -e development`
 5. Edit phinx.yml with your db details
 6. `vendor/bin/phinx migrate -e development` (or other environment name)
+7. Seed database using `vendor/bin/phinx seed:run`
 
 If you have issues, https://stackoverflow.com/a/25782795/3938401 might be helpful, and you might want to use 127.0.0.1 instead of `localhost`
---
+++ db/seeds/DefaultSeeder.php
@@ -0,0 +1,89 @@
+<?php
+
+
+use Phinx\Seed\AbstractSeed;
+
+class DefaultSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $userTypeData = [
+            [
+                'Type' => 'Guest',
+                'DisplayName' => 'Guest'
+            ],
+            [
+                'Type' => 'Pathfinder',
+                'DisplayName' => 'Pathfinder'
+            ],
+            [
+                'Type' => 'ClubAdmin',
+                'DisplayName' => 'Club Administrator'
+            ],
+            [
+                'Type' => 'ConferenceAdmin',
+                'DisplayName' => 'Conference Administrator'
+            ],
+            [
+                'Type' => 'WebAdmin',
+                'DisplayName' => 'Website Administrator'
+            ]
+        ];
+        $userTypes = $this->table('UserTypes');
+        $userTypes->insert($userTypeData)->save();
+
+        $settingsData = [
+            [
+                'SettingKey' => 'AboutContactName',
+                'SettingValue' => '-',
+                'DisplayName' => 'About Page - Contact Name'
+            ],
+            [
+                'SettingKey' => 'AboutContactEmail',
+                'SettingValue' => '-',
+                'DisplayName' => 'About Page - Contact Email'
+            ],
+            [
+                'SettingKey' => 'WebsiteName',
+                'SettingValue' => 'PBE Quiz Engine',
+                'DisplayName' => 'Website Name'
+            ],
+            [
+                'SettingKey' => 'WebsiteTabTitle',
+                'SettingValue' => 'PBE',
+                'DisplayName' => 'Website Title (Browser Tab)'
+            ],
+            [
+                'SettingKey' => 'AnalyticsURL',
+                'SettingValue' => '//babien.co/analytics/',
+                'DisplayName' => 'Analytics URL'
+            ],
+            [
+                'SettingKey' => 'AnalyticsSiteID',
+                'SettingValue' => '1',
+                'DisplayName' => 'Analytics Site ID'
+            ],
+        ];
+        $settings = $this->table('Settings');
+        $settings->insert($settingsData)->save();
+
+        $conferencesData = [
+            [
+                'Name' => 'UCC',
+                'URL' => 'https://uccsda.org/English/Pages/HomePage.aspx',
+                'ContactName' => '-',
+                'ContactEmail' => '-'
+            ],
+        ];
+        $conferences = $this->table('Conferences');
+        $conferences->insert($conferencesData)->save();
+    }
+}
