857b7982a598943da7a6209249b8772d30eb33a7
Deadpikle <mikebobby@sbcglobal.net>
2018-06-25 20:41:25 -0400

    Finish up deleting fill in Bible questions

---
+++ admin/delete-bible-fill.php
@@ -0,0 +1,86 @@
+<?php
+    require_once(dirname(__FILE__)."/init-admin.php");
+    
+    $title = 'Delete Bible Fill In Questions';
+
+    if (!$isWebAdmin) {
+        header("Location: $basePath/index.php");
+        die();
+    }
+    // type can be "chapter" (deleting just one chapter's worth of questions) or "all" (deleting all questions)
+    $type = $_GET["type"];
+
+    if ($type === "chapter") {
+        $chapterID = $_GET["id"];
+        $query = '
+            SELECT Name, Number
+            FROM Books b JOIN Chapters c ON b.BookID = c.BookID
+            WHERE ChapterID = ?';
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$chapterID]);
+        $data = $stmt->fetch();
+        if ($data == NULL) {
+            die("Invalid chapter id");
+        }
+        $warning = "Are you sure you want to delete all of the Bible fill in the blank questions for <b>" . $data['Name'] . " Chapter " . $data["Number"] . "</b>?";
+    }
+    else if ($type === "all") {
+        $chapterID = -1;
+        $warning = "Are you sure you want to delete <b>all</b> Bible fill in the blank questions?";
+    }
+    else {
+        // ?? invalid data
+        header("Location: $basePath/index.php");
+        die();
+    }
+    
+    if ($isPostRequest && $type == $_POST["type"]) {
+        $currentYear = get_active_year($pdo)["YearID"];
+        // the weird subquery SELECT * was due to a workaround
+        // for the error discussed here: https://stackoverflow.com/q/44970574/3938401
+        if ($type === "all") {
+            $query = 'DELETE FROM Questions
+                      WHERE QuestionID IN (
+                        SELECT q.QuestionID 
+                        FROM (SELECT * FROM Questions) q JOIN Verses v ON q.StartVerseID = v.VerseID
+                            JOIN Chapters c ON c.ChapterID = v.ChapterID
+                            JOIN Books b ON b.BookID = c.BookID
+                        WHERE q.Type = "bible-qna-fill"
+                            AND b.YearID = ?)';
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([$currentYear]);
+            header("Location: view-bible-fill-in.php");
+            die();
+        }
+        else if ($type === "chapter" && $chapterID == $_POST["chapter-id"]) {
+            $query = 'DELETE FROM Questions 
+                      WHERE QuestionID IN (
+                        SELECT q.QuestionID 
+                        FROM (SELECT * FROM Questions) q JOIN Verses v ON q.StartVerseID = v.VerseID
+                            JOIN Chapters c ON c.ChapterID = v.ChapterID
+                            JOIN Books b ON b.BookID = c.BookID
+                        WHERE c.ChapterID = ? AND q.Type = "bible-qna-fill"
+                            AND b.YearID = ?)';
+            $stmt = $pdo->prepare($query);
+            $stmt->execute([$chapterID, $currentYear]);
+            header("Location: view-bible-fill-in.php");
+            die();
+        }
+    }
+
+?>
+
+<?php include(dirname(__FILE__)."/../header.php"); ?>
+
+<p><a class="btn-flat blue-text waves-effect waves-blue no-uppercase" href="./view-bible-fill-in.php">Back</a></p>
+
+<div id="delete-book">
+    <h4><?= $warning ?></h4>
+    <form method="post">
+        <input type="hidden" name="type" value="<?= $type ?>"/>
+        <input type="hidden" name="chapter-id" value="<?= $chapterID ?>"/>
+        <button class="btn waves-effect waves-light submit red white-text" type="submit" name="action">Delete Questions</button>
+    </form>
+</div>
+
+<?php include(dirname(__FILE__)."/../footer.php"); ?>
--- admin/view-bible-fill-in.php
+++ admin/view-bible-fill-in.php
@@ -9,14 +9,14 @@
     }
     $currentYear = get_active_year($pdo)["YearID"];
     $query = '
-        SELECT b.BookID, b.Name, COUNT(q.QuestionID) AS QuestionCount
+        SELECT c.ChapterID, c.Number, b.Name, COUNT(q.QuestionID) AS QuestionCount
         FROM Questions q JOIN Verses v ON q.StartVerseID = v.VerseID 
             JOIN Chapters c ON c.ChapterID = v.ChapterID
             JOIN Books b ON b.BookID = c.BookID
         WHERE b.YearID = ' . $currentYear . ' 
               AND q.Type = "bible-qna-fill"
-        GROUP BY b.BookID
-        ORDER BY b.Name';
+        GROUP BY c.ChapterID, c.Number
+        ORDER BY b.Name, c.Number';
     $stmt = $pdo->prepare($query);
     $stmt->execute([]);
     $bookQuestionData = $stmt->fetchAll();
@@ -25,6 +25,7 @@
     foreach ($bookQuestionData as $item) {
         $total += (int)$item['QuestionCount'];
     }
+    $questionString = $total == 1 ? 'question' : 'questions';
 ?>
 
 <?php include(dirname(__FILE__)."/../header.php"); ?>
@@ -33,29 +34,33 @@
 
 <h4>Bible Fill in the Blank Questions</h4>
 
-<p>There are a total of <b><?= $total ?></b> Bible fill in the blank questions in the system out of a maximum of 500.</p>
+<p>There are a total of <b><?= $total ?></b> Bible fill in the blank <?= $questionString ?> in the system out of a maximum of 500.</p>
 
-<div id="bible-fill-in-div">
-    <table class="striped">
-        <thead>
-            <tr>
-                <th>Book</th>
-                <th>Number of Questions</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($bookQuestionData as $data) { ?>
+<?php if ($total > 0) { ?>
+    <div id="bible-fill-in-div">
+        <table class="striped">
+            <thead>
                 <tr>
-                    <td><b><?= $data['Name'] ?></b></td>
-                    <td><?= $data['QuestionCount'] ?> questions</li></td>
-                    <td><a class="waves-effect waves-light btn red white-text" href="delete-bible-fill.php?type=book&id=<?= $data['BookID'] ?>">Delete</a></td>
+                    <th>Chapter</th>
+                    <th>Number of Fill In Questions</th>
+                    <th></th>
                 </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-    <a class="waves-effect waves-light btn red white-text" href="delete-bible-fill.php?type=all">Delete All Bible Fill In Questions</a>
-</div>
+            </thead>
+            <tbody>
+                <?php foreach ($bookQuestionData as $data) { ?>
+                    <?php $questionString = $data['QuestionCount'] == 1 ? 'question' : 'questions'; ?>
+                    <tr>
+                        <td><b><?= $data['Name'] ?>&nbsp;<?= $data['Number'] ?></b></td>
+                        <td><?= $data['QuestionCount'] ?> <?= $questionString ?></li></td>
+                        <td><a class="waves-effect waves-light btn red white-text" href="delete-bible-fill.php?type=chapter&id=<?= $data['ChapterID'] ?>">Delete</a></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+        <div class="divider"></div>
+        <a class="waves-effect waves-light btn red white-text" href="delete-bible-fill.php?type=all">Delete All Bible Fill In Questions</a>
+    </div>
+<?php } ?>
 
 <script type="text/javascript">
     $(document).ready(function() {
