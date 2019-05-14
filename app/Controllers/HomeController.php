4eca1d2118897963441cbe1e4ae011ea4aa2a515
Michael Babienco <mbabienco@southern.edu>
2019-05-14 11:35:17 +0700

    Fix no redirect if not logged in

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -14,7 +14,6 @@ class HomeController
     public function index(PBEAppConfig $app, Request $request)
     {
         if (!$app->loggedIn) {
-            return new View('home/index', compact('sections'), 'Cow');
             return new Redirect('login');
         }
         $title = 'Home';
@@ -40,35 +39,35 @@ class HomeController
             WHERE EntryCode = ?';
         $stmt = $app->db->prepare($query);
         $params = [
-            $request->post["access-code"]
+            $request->post['access-code']
         ];
         $stmt->execute($params);
         if ($row = $stmt->fetch()) {
             // Login success!
             // Update the database
-            $updateQuery = 'UPDATE Users SET LastLoginDate = ? WHERE UserID = ' . $row["UserID"];
+            $updateQuery = 'UPDATE Users SET LastLoginDate = ? WHERE UserID = ' . $row['UserID'];
             $statement = $app->db->prepare($updateQuery);
             $params = [
-                date("Y-m-d H:i:s")
+                date('Y-m-d H:i:s')
             ];
             $statement->execute($params);
             // Update the session
-            $_SESSION["UserID"] = $row["UserID"];
-            $_SESSION["Username"] = $row["Username"];
-            $_SESSION["UserType"] = $row["UserType"];
-            if ($row["ClubID"] != NULL) {
-                $_SESSION["ClubID"] = $row["ClubID"];
+            $_SESSION['UserID'] = $row['UserID'];
+            $_SESSION['Username'] = $row['Username'];
+            $_SESSION['UserType'] = $row['UserType'];
+            if ($row['ClubID'] != NULL) {
+                $_SESSION['ClubID'] = $row['ClubID'];
             } else {
-                $_SESSION["ClubID"] = -1;
+                $_SESSION['ClubID'] = -1;
             }
-            $_SESSION["ClubName"] = $row["ClubName"];
-            if ($row["ConferenceID"] != NULL) {
-                $_SESSION["ConferenceID"] = $row["ConferenceID"];
+            $_SESSION['ClubName'] = $row['ClubName'];
+            if ($row['ConferenceID'] != NULL) {
+                $_SESSION['ConferenceID'] = $row['ConferenceID'];
             } else {
-                $_SESSION["ConferenceID"] = -1;
+                $_SESSION['ConferenceID'] = -1;
             }
-            $_SESSION["ConferenceName"] = $row["ConferenceName"];
-            $_SESSION["PreferredLanguageID"] = $row["PreferredLanguageID"];
+            $_SESSION['ConferenceName'] = $row['ConferenceName'];
+            $_SESSION['PreferredLanguageID'] = $row['PreferredLanguageID'];
             return new Redirect('/');
         } else {
             $error = 'Invalid access code';
