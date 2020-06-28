1eb5f7c174911bce032a25fedaf6c3861dc4f75c
Michael Babienco <mbabienco@southern.edu>
2020-06-28 13:30:19 -0400

    bye bye old admin files

--- admin/ajax/import-home-info-from-admin.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require_once(dirname(__FILE__)."/../init-admin.php");
-    $toConferenceID = $_POST["to-conference-id"];
-    copy_home_sections($pdo, get_web_admin_conference_id($pdo), $toConferenceID, $_POST["year"]);
-    header("Location: $basePath/admin/view-home-sections.php?conferenceID=" . $toConferenceID);
-?>
--- admin/ajax/import-home-info-from-conference.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once(dirname(__FILE__)."/../init-admin.php");
-    $fromConferenceID = $_POST["from-conference-id"];
-    $toConferenceID = $_POST["to-conference-id"];
-    copy_home_sections($pdo, $fromConferenceID, $toConferenceID, $_POST["year"]);
-    header("Location: $basePath/admin/view-home-sections.php?conferenceID=" . $toConferenceID);
-?>
