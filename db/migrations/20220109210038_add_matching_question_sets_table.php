8804bc1c40c971163ad07e21daa99f6be4e5c785
Michael Babienco <mbabienco@southern.edu>
2022-01-09 16:07:01 -0500

    Add migrations for matching questions

---
+++ db/migrations/20220109210038_add_matching_question_sets_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddMatchingQuestionSetsTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('MatchingQuestionSets', ['id' => 'MatchingQuestionSetID', 'collation'=>'utf8mb4_unicode_ci']);
+        $table->addColumn('Name', 'string', ['limit' => 100]);
+        $table->addColumn('Description', 'string', ['limit' => 500]);
+        $table->addColumn('LanguageID', 'integer', ['null' => true, 'default' => 1]);
+        $table->addColumn('YearID', 'integer', ['null' => false, 'default' => 1]);
+        $table->addForeignKey('LanguageID', 'Languages', 'LanguageID', ['delete'=> 'SET_NULL', 'update' => 'NO_ACTION']);
+        $table->addForeignKey('YearID', 'Years', 'YearID', ['delete'=> 'CASCADE', 'update' => 'NO_ACTION']);
+        $table->create();
+    }
+}
---
+++ db/migrations/20220109210302_add_matching_question_item_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddMatchingQuestionItemTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Any other destructive changes will result in an error when trying to
+     * rollback the migration.
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('MatchingQuestionItems', ['id' => 'MatchingQuestionItemID', 'collation'=>'utf8mb4_unicode_ci']);
+        $table->addColumn('Question', 'string', ['limit' => 250]);
+        $table->addColumn('Answer', 'string', ['limit' => 1000]);
+        $table->addColumn('DateCreated', 'datetime', ['default' => 'CURRENT_TIMESTAMP']);
+        $table->addColumn('DateModified', 'datetime', ['default' => 'CURRENT_TIMESTAMP']);
+        $table->addColumn('IsDeleted', 'boolean', ['default' => false]);
+
+        $table->addColumn('CreatorID', 'integer', ['null' => true]);
+        $table->addColumn('LastEditedByID', 'integer', ['null' => true]);
+        $table->addColumn('StartVerseID', 'integer', ['null' => true]);
+        $table->addColumn('EndVerseID', 'integer', ['null' => true]);
+
+        $table->addForeignKey('CreatorID', 'Users', 'UserID', ['delete'=> 'SET_NULL', 'update' => 'NO_ACTION']);
+        $table->addForeignKey('LastEditedByID', 'Users', 'UserID', ['delete'=> 'SET_NULL', 'update' => 'NO_ACTION']);
+        $table->addForeignKey('StartVerseID', 'Verses', 'VerseID', ['delete'=> 'SET_NULL', 'update' => 'NO_ACTION']);
+        $table->addForeignKey('EndVerseID', 'Verses', 'VerseID', ['delete'=> 'SET_NULL', 'update' => 'NO_ACTION']);
+
+        $table->create();
+    }
+}
