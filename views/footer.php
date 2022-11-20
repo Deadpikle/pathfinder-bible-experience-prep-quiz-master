28e7e7f8687a118d83b4d14a378e246253316f7f
Michael Babienco <mbabienco@southern.edu>
2022-11-20 15:51:27 -0500

    Update favicon, various static text

--- views/footer.php
+++ views/footer.php
@@ -3,7 +3,7 @@
                 </div>
             </main>
         <footer class="container">
-            <p><a class="" href="<?= $app->yurl('/about') ?>"><?= $app->websiteName ?></a> © 2022. Favicon: <a href="https://thenounproject.com/term/bible/22633/" target="_blank">Bible</a> by Dan Hetteix from <a href="http://thenounproject.com/" target="_blank">the Noun Project</a>.<br><?= $app->footerText ?></p>
+            <p><a class="" href="<?= $app->yurl('/about') ?>"><?= $app->websiteName ?></a> © 2022.<br><?= $app->footerText ?></p>
         </footer>
     </body>
     <script type="text/javascript">
