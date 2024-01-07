a31a776b2cdc6f5529b550392c097ea6b864aabc
Michael Babienco <mbabienco@southern.edu>
2024-01-07 10:11:53 +0900

    Fix questions at start of fill in

--- app/Models/Util.php
+++ app/Models/Util.php
@@ -83,7 +83,7 @@ class Util
             $output = self::fixQuestionMarkOnQuestion($output);
         }
         $needToAddFirstQMark = false;
-        if (YamfUtil::strStartsWith($output, '¿') || $languageAbbr === 'es') {
+        if (YamfUtil::strStartsWith($output, '¿') || ($languageAbbr === 'es' && !$isFillIn)) {
             $output = trim($output, '¿');
             $needToAddFirstQMark = true;
         }
